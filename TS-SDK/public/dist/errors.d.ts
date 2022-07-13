18e3f328b78f232ae637233cf83ddfe1f9df2db7
Amir Hamza <amir@educationequation.co.uk>
2022-07-13 18:07:34 +0100

    remove dist

--- TS-SDK/public/dist/errors.d.ts
+++
@@ -1,3 +0,0 @@
-import { Program } from "@project-serum/anchor";
-export declare const parseError: (e: any, p: Program) => any;
-export declare const getAverIdlErrors: (p: Program) => Map<any, any>;
