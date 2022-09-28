008c7f72aed482a0e6ee12b2cedcda011787779f
Amir Hamza <amir@educationequation.co.uk>
2022-09-28 19:34:15 +0200

    Bug fix

--- PY-SDK/public/setup.py
+++ PY-SDK/public/setup.py
@@ -15,7 +15,7 @@ class build_py(build_py_orig):
 
 setup(
     name='pyaver',
-    version='0.0.29',
+    version='0.0.07',
     license='MIT',
     author="Aver Ramanujan",
     author_email='email@example.com',
